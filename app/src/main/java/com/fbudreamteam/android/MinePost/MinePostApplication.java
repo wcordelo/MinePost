acb78e4bdd9b6fae6695fed53b35c36f2f968887
Karinna Loo <karinnaloo@fb.com>
2015-07-09 11:09:29 -0700

    app login works now.. working on logout next

---
+++ app/src/main/java/com/fbudreamteam/android/MinePost/MinePostApplication.java
@@ -0,0 +1,35 @@
+package com.fbudreamteam.android.MinePost;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseCrashReporting;
+import com.parse.ParseFacebookUtils;
+
+/**
+ * Created by karinnaloo on 7/9/15.
+ */
+public class MinePostApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Initialize Crash Reporting.
+        ParseCrashReporting.enable(this);
+
+        // Enable Local Datastore.
+        Parse.enableLocalDatastore(this);
+
+        // Add your initialization code here
+        Parse.initialize(this, "aBsY5STXm1XVwrQBMLyE5qh8iSSQuOhagHffyE1b", "uNVlKv0ZuC7fyEjMuKg7eUtQvEAEY2b9uQZ9yGbt");
+
+        //ParseFacebookUtils.initialize(this);
+
+        ParseACL defaultACL = new ParseACL();
+        // Optionally enable public read access.
+        // defaultACL.setPublicReadAccess(true);
+        ParseACL.setDefaultACL(defaultACL, true);
+    }
+}
