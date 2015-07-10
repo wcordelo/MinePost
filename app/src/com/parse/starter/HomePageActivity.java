8ce67014e33f5dc3e6b28be3b1fff3b14f09197b
Karinna Loo <karinnaloo@fb.com>
2015-07-10 14:05:18 -0700

    camera working; saves images to parse

--- app/src/com/parse/starter/HomePageActivity.java
+++ app/src/com/parse/starter/HomePageActivity.java
@@ -3,6 +3,8 @@ package com.parse.starter;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 
@@ -10,8 +12,12 @@ import com.parse.ParseAnalytics;
 import com.parse.ParseUser;
 
 public class HomePageActivity extends Activity {
-    Button logOut;
-    Button create;
+    private Button logOut;
+    private Button create;
+    //Required for RecyclerView
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     /**
      * Called when the activity is first created.
@@ -21,6 +27,14 @@ public class HomePageActivity extends Activity {
         setContentView(R.layout.fragment_home_page);
 
         ParseAnalytics.trackAppOpenedInBackground(getIntent());
+
+        //RecyclerView
+        mRecyclerView = (RecyclerView) findViewById(R.id.minepost_recycler_view);
+        mRecyclerView.setHasFixedSize(true); //keeps the RecyclerView Fixed in Size
+        mLayoutManager = new LinearLayoutManager(this); //RecyclerView needs a Layout Manager
+        mRecyclerView.setLayoutManager(mLayoutManager); //Layout Manager
+//        mAdapter = new MyAdapter(myDataset); //RecyclerView Adapter
+//        mRecyclerView.setAdapter(mAdapter); //RecyclerView Adapter
 
         logOut = (Button) findViewById(R.id.logout);
         //log out button
@@ -50,4 +64,53 @@ public class HomePageActivity extends Activity {
         });
 
     }
+
+//    public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+//        private String[] mDataset;
+//
+//        // Provide a reference to the views for each data item
+//        // Complex data items may need more than one view per item, and
+//        // you provide access to all the views for a data item in a view holder
+//        public static class ViewHolder extends RecyclerView.ViewHolder {
+//            // each data item is just a string in this case
+//            public TextView mTextView;
+//            public ViewHolder(TextView v) {
+//                super(v);
+//                mTextView = v;
+//            }
+//        }
+
+        // Provide a suitable constructor (depends on the kind of dataset)
+//        public MyAdapter(String[] myDataset) {
+//            mDataset = myDataset;
+//        }
+//
+//        // Create new views (invoked by the layout manager)
+//        @Override
+//        public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+//                                                       int viewType) {
+//            // create a new view
+//            View v = LayoutInflater.from(parent.getContext())
+//                    .inflate(R.layout.recycler_view_item, parent, false);
+//            // set the view's size, margins, paddings and layout parameters
+//
+//            ViewHolder vh = new ViewHolder((TextView) v);
+//            return vh;
+//        }
+//
+//        // Replace the contents of a view (invoked by the layout manager)
+//        @Override
+//        public void onBindViewHolder(ViewHolder holder, int position) {
+//            // - get element from your dataset at this position
+//            // - replace the contents of the view with that element
+//            holder.mTextView.setText(mDataset[position]);
+//
+//        }
+//
+//        // Return the size of your dataset (invoked by the layout manager)
+//        @Override
+//        public int getItemCount() {
+//            return mDataset.length;
+//        }
+//    }
 }
