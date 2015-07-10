a98a4d366c428e77b1ad38c518616393ece62b63
Alexander Mendelsohn <alexjmendelsohn@fb.com>
2015-07-10 14:08:52 -0700

    small change to ViewPostActivity

--- app/src/com/parse/starter/ViewPostActivity.java
+++ app/src/com/parse/starter/ViewPostActivity.java
@@ -11,9 +11,10 @@ public class ViewPostActivity extends Activity{
     TextView title;
     ImageView image;
     ImageButton upButton;
-    TextView votes;
+    TextView votesView;
     ImageButton downButton;
     TextView description;
+    int votes;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,9 +23,10 @@ public class ViewPostActivity extends Activity{
         title = (TextView) findViewById(R.id.MinePost_view_title);
         image = (ImageView) findViewById(R.id.MinePost_view_photo);
         upButton = (ImageButton) findViewById(R.id.up_vote_button);
-        votes = (TextView) findViewById(R.id.votes_counter);
+        votesView = (TextView) findViewById(R.id.votes_counter);
         downButton = (ImageButton) findViewById(R.id.down_vote_button);
         description = (TextView) findViewById(R.id.description_text_view);
+
 
 
     }
