e7d7bb54b177e05a987e8ae53a1a1a34c8d39af8
Alexander Mendelsohn <alexjmendelsohn@fb.com>
2015-07-10 14:04:22 -0700

    adding up and down vote buttons and ViewPostActivity

---
+++ app/src/com/parse/starter/ViewPostActivity.java
@@ -0,0 +1,34 @@
+package com.parse.starter;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+public class ViewPostActivity extends Activity{
+    TextView title;
+    ImageView image;
+    ImageButton upButton;
+    TextView votes;
+    ImageButton downButton;
+    TextView description;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_view_post);
+
+        title = (TextView) findViewById(R.id.MinePost_view_title);
+        image = (ImageView) findViewById(R.id.MinePost_view_photo);
+        upButton = (ImageButton) findViewById(R.id.up_vote_button);
+        votes = (TextView) findViewById(R.id.votes_counter);
+        downButton = (ImageButton) findViewById(R.id.down_vote_button);
+        description = (TextView) findViewById(R.id.description_text_view);
+
+
+    }
+
+
+
+}
