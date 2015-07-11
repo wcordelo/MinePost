72e33d137e379f459c4668220da194eb891704c7
Alexander Mendelsohn <alexjmendelsohn@fb.com>
2015-07-10 17:02:52 -0700

    added temporary back button to ViewPostActivity

--- app/src/com/parse/starter/ViewPostActivity.java
+++ app/src/com/parse/starter/ViewPostActivity.java
@@ -2,12 +2,14 @@ package com.parse.starter;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 
 public class ViewPostActivity extends Activity{
+    ImageButton backButton;
     TextView title;
     ImageView image;
     ImageButton upButton;
@@ -20,12 +22,20 @@ public class ViewPostActivity extends Activity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_view_post);
 
+        backButton = (ImageButton) findViewById(R.id.view_post_back_button);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
         title = (TextView) findViewById(R.id.MinePost_view_title);
         image = (ImageView) findViewById(R.id.MinePost_view_photo);
         upButton = (ImageButton) findViewById(R.id.up_vote_button);
         votesView = (TextView) findViewById(R.id.votes_counter);
         downButton = (ImageButton) findViewById(R.id.down_vote_button);
         description = (TextView) findViewById(R.id.description_text_view);
+
 
 
 
