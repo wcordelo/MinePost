e0a231f7b732f840f6c1140069ed0d9dfe606fbf
Alexander Mendelsohn <alexjmendelsohn@fb.com>
2015-07-13 10:57:16 -0700

    changed up/down votes to like button

--- app/src/com/parse/starter/ViewPostActivity.java
+++ app/src/com/parse/starter/ViewPostActivity.java
@@ -8,39 +8,46 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 
-public class ViewPostActivity extends Activity{
+public class ViewPostActivity extends Activity {
     ImageButton backButton;
     TextView title;
     ImageView image;
-    ImageButton upButton;
+    ImageButton likeButton;
     TextView votesView;
-    ImageButton downButton;
     TextView description;
-    int votes;
+    int votes = 0;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_view_post);
 
         backButton = (ImageButton) findViewById(R.id.view_post_back_button);
+        title = (TextView) findViewById(R.id.MinePost_view_title);
+        image = (ImageView) findViewById(R.id.MinePost_view_photo);
+        likeButton = (ImageButton) findViewById(R.id.like_button);
+        votesView = (TextView) findViewById(R.id.votes_counter);
+        description = (TextView) findViewById(R.id.description_text_view);
+
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
             }
         });
-        title = (TextView) findViewById(R.id.MinePost_view_title);
-        image = (ImageView) findViewById(R.id.MinePost_view_photo);
-        upButton = (ImageButton) findViewById(R.id.up_vote_button);
-        votesView = (TextView) findViewById(R.id.votes_counter);
-        downButton = (ImageButton) findViewById(R.id.down_vote_button);
-        description = (TextView) findViewById(R.id.description_text_view);
+
+
+        likeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                votes++;
+                likeButton.setEnabled(false);
+            }
+        });
 
 
 
 
     }
-
 
 
 }
